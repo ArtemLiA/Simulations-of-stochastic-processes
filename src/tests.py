392a2fd8a5a375529dd2b93f54274c5d9884a472
Artem Liakhov <Liakhov.Artem@yandex.ru>
2023-11-12 22:31:06 +0300

    Added test of show_trajectory function

--- src/tests.py
+++ src/tests.py
@@ -9,7 +9,6 @@ v0 = np.array([1.0, 0.0, 0.0])
 states = np.array(['a', 'b', 'c'])
 
 mc = MarkovChain(v0, TPM)
-mc.set_states(states)
 mc.set_initial_probabilities(np.array([0.5, 0.3, 0.2]))
 
 print("Init probs:", mc.get_initial_probabilities(), sep="\n")
@@ -18,5 +17,8 @@ print("States:", mc.get_states(), sep="\n")
 
 d1 = mc.empirical_distribution(5, 150)
 d2 = mc.theoretical_distribution(5)
+trj = mc.simulate_trajectories(10)
 
 show_distributions(d1, d2, "Example title", "d1", "d2")
+show_trajectory(trj, "Some title")
+
