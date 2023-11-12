383e234d5885741ff0e65d7718e7bf7264040285
Artem Liakhov <Liakhov.Artem@yandex.ru>
2023-11-12 23:04:37 +0300

    Minor changes

--- src/tests.py
+++ src/tests.py
@@ -2,14 +2,15 @@
 from mc import *
 from views import *
 
-TPM = np.array([[1/3, 1/3, 1/3], 
-                [1/3, 1/3, 1/3],
-                [1/3, 1/3, 1/3]])
-v0 = np.array([1.0, 0.0, 0.0])
+TPM = np.array([[0.0, 0.0, 1.0, 0.0, 0.0], 
+                [1.0, 0.0, 0.0, 0.0, 0.0], 
+                [0.4, 0.6, 0.0, 0.0, 0.0], 
+                [0.1, 0.4, 0.2, 0.3, 0.0],
+                [0.0, 0.0, 0.0, 0.5, 0.5]])
+v0 = np.array([1/3, 1/3, 0.0, 0.0, 1/3])
 states = np.array(['a', 'b', 'c'])
 
 mc = MarkovChain(v0, TPM)
-mc.set_initial_probabilities(np.array([0.5, 0.3, 0.2]))
 
 print("Init probs:", mc.get_initial_probabilities(), sep="\n")
 print("Transition matrix:", mc.get_transition_matrix(), sep="\n")
@@ -19,6 +20,5 @@ d1 = mc.empirical_distribution(5, 150)
 d2 = mc.theoretical_distribution(5)
 trj = mc.simulate_trajectories(10)
 
-show_distributions(d1, d2, "Example title", "d1", "d2")
-show_trajectory(trj, "Some title")
+show_distributions(d1, d2, "Example title", "Empirical", "Theoretical")
 
